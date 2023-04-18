50f3a68e6b1a53e2430454a4abdfa0808c3e0539
Ishan Vyas <ishan.vyas@duke.edu>
2023-04-18 19:49:24 -0400

    blank message

--- APT 9/Ograph.java
+++ APT 9/Ograph.java
@@ -1,3 +1,12 @@
+import java.util.*;
+
 public class Ograph {
-    
+    public int[] components(String[] data) {
+        HashMap<Integer, HashSet<Integer>> connComps = new HashMap<>();
+        for (int i=0;i<data.length;i++) {
+            String[] nums = data[i].split(" ");
+            
+        }
+        return null;
+    }
 }
